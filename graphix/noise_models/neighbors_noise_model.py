be9b70917642b6439b42cd47f9e99528e5d90a21
Benjamin Guichard <benjamin.guichard@inria.fr>
2024-12-20 14:05:58 +0100

    specify a dict containing the krauss channels to apply at the initialization of the noise model + input graph + input_nodes method

--- graphix/noise_models/neighbors_noise_model.py
+++ graphix/noise_models/neighbors_noise_model.py
@@ -18,49 +18,55 @@ class NeighborsNoiseModel(NoiseModel):
     """Neighbor noise model.
 
     Tracks the neighbor nodes of each nodes using a state graph.
-    The Krauss channel that is applied to the targetted nodes should be specified when initializing the NeighborsNoiseModel.
+    Apply channels specified in `channel_selector`.
+    The entries within `channel_selector` are CommandKind.
     """
 
     def __init__(
-        self,
-        one_qubit_channel: function,
-        two_qubits_channel: function,
-        prepare_error_prob: float = 0.0,
-        x_error_prob: float = 0.0,
-        z_error_prob: float = 0.0,
-        entanglement_error_prob: float = 0.0,
-        measure_channel_prob: float = 0.0,
-        measure_error_prob: float = 0.0,
-        rng: Generator = None,
+        self, channel_specifier: dict[str, KrausChannel], rng: Generator = None, input_graph: nx.Graph = None
     ) -> None:
-        self._state_graph: nx.Graph = nx.Graph()
-        self.one_qubit_channel = one_qubit_channel
-        self.two_qubit_channel = two_qubits_channel
-        self.prepare_error_prob = prepare_error_prob
-        self.x_error_prob = x_error_prob
-        self.z_error_prob = z_error_prob
-        self.entanglement_error_prob = entanglement_error_prob
-        self.measure_error_prob = measure_error_prob
-        self.measure_channel_prob = measure_channel_prob
+        self._state_graph: nx.Graph = input_graph  # Global tracking of the neighbors (entanglement and position)
         self.rng = ensure_rng(rng)
+        self.channel_specifier = channel_specifier
 
     def _get_neighbors(self, nodes: list[int]) -> list[int]:
-        """Return the neighbors of multiple nodes"""
+        """Return the neighbors of multiple nodes."""
         return [neighbor for n in nodes for neighbor in self._state_graph.neighbors(n)]
 
-    def input_nodes(self, nodes: list[int]) -> Noise:
-        """Return the noise to apply to input nodes and their neighbor nodes.
+    def input_nodes(
+        self,
+        nodes: list[int],
+    ) -> Noise:
+        """Return the noise to apply to the input nodes' neighbors.
 
-        We assume that the noise is applied to every neighbors,
-        regardless the fact that one node can be the neighbor of
-        two other nodes.
-        The noise will be applied as many time as it the node is
-        the neighbor of other nodes.
+        For each nodes, check if they have neighbors.
+        If so, compose the nodes on which the channel will be applied
+        according to its number of qubits.
         """
-        self._state_graph.add_nodes_from(nodes)  # Add initial nodes into the graph
-        return [(self.one_qubit_channel(self.prepare_error_prob), [node]) for node in nodes] + [
-            (self.one_qubit_channel(self.prepare_error_prob), [neighbor]) for neighbor in self._get_neighbors(nodes)
-        ]
+        if self._state_graph is None:
+            self._state_graph = nx.Graph(nodes)
+
+        channel = self.channel_specifier["input"]
+        noise = Noise()
+        for n in nodes:  # ITerate through each nodes
+            neighbors = self._state_graph.neighbors(n)  # Get neighbors
+            if len(neighbors) == 0:
+                continue
+            channel_nqubits = channel.nqubit
+            target_neighbors = []
+
+            assert channel_nqubits <= len(
+                neighbors
+            ), f"Krauss channel with {channel_nqubits} qubits can not be applied to {len(neighbors) + 1} qubits."
+
+            for i in range(
+                len(neighbors) - channel_nqubits + 1
+            ):  # Compose targets according to the channel's number of qubits
+                target_neighbors += tuple(neighbors[i : i + channel_nqubits - 1])
+
+            noise.extend([(channel, list(target)) for target in target_neighbors])  # Update noise
+
+        return noise
 
     def command(self, cmd: Command) -> Noise:
         """Return the noise to apply to the command `cmd`.
@@ -71,43 +77,40 @@ class NeighborsNoiseModel(NoiseModel):
         """
         kind = cmd.kind
         if kind == CommandKind.N:
-            self._state_graph.add_node(cmd.node)
-            return [(self.one_qubit_channel(self.prepare_error_prob), [cmd.node])] + [
-                (self.one_qubit_channel(self.prepare_error_prob), [neighbor])
-                for neighbor in self._get_neighbors([cmd.node])
-            ]
+            if cmd.node not in self._state_graph:
+                self._state_graph.add_node(cmd.node)
+
+            neighbors = self._get_neighborshbors([cmd.node])
+
+            channel = self.channel_specifier["N"]
+            channel_nqubits = channel.nqubit
+
+            target_neighbors = []
+
+            assert channel_nqubits <= len(
+                neighbors
+            ), f"Krauss channel with {channel_nqubits} qubits can not be applied to {len(neighbors) + 1} qubits."
+
+            for i in range(
+                len(neighbors) - channel_nqubits + 1
+            ):  # Compose targets according to the channel's number of qubits
+                target_neighbors += tuple(neighbors[i : i + channel_nqubits - 1])
 
         if kind == CommandKind.E:
-            self._state_graph.add_edge(*cmd.nodes)
-            return (
-                [(self.two_qubit_channel(self.entanglement_error_prob), list(cmd.nodes))]
-                + [
-                    (self.two_qubit_channel(self.entanglement_error_prob), [cmd.nodes[0], neighbor])
-                    for neighbor in self._get_neighbors([cmd.nodes[0]])
-                ]
-                + [
-                    (self.two_qubit_channel(self.entanglement_error_prob), [cmd.nodes[1], neighbor])
-                    for neighbor in self._get_neighbors([cmd.nodes[1]])
-                ]
-            )
+            # TODO
+            return []
 
         if kind == CommandKind.M:
-            noise = [(self.one_qubit_channel(self.measure_channel_prob), [cmd.node])] + [
-                (self.one_qubit_channel(self.measure_channel_prob), [neighbor])
-                for neighbor in self._get_neighbors([cmd.node])
-            ]
-            self._state_graph.remove_node(cmd.node)
-            return noise
+            # TODO
+            return []
 
         if kind == CommandKind.X:
-            return [(self.one_qubit_channel(self.x_error_prob), [cmd.node])] + [
-                (self.one_qubit_channel(self.x_error_prob), [neighbor]) for neighbor in self._get_neighbors([cmd.node])
-            ]
+            # TODO
+            return []
 
         if kind == CommandKind.Z:
-            return [(self.one_qubit_channel(self.z_error_prob), [cmd.node])] + [
-                (self.one_qubit_channel(self.z_error_prob), [neighbor]) for neighbor in self._get_neighbors([cmd.node])
-            ]
+            # TODO
+            return []
 
         if kind == CommandKind.C or kind == CommandKind.T:
             return [(KrausChannel([]), [])]
