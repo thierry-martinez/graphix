ca9f217ac496fade51138a65dfc25d34ceb86d46
Thierry Martinez <thierry.martinez@inria.fr>
2025-03-11 17:15:04 +0100

    ruff format

--- tests/test_states.py
+++ tests/test_states.py
@@ -2,9 +2,11 @@ import pytest
 
 from graphix.states import BasicState
 
+
 @pytest.mark.parametrize("bs", list(BasicState))
 def test_try_from_statevector(bs: BasicState):
     assert BasicState.try_from_statevector(bs.value.get_statevector()) == bs
+
 
 def test_try_from_statevector_fail():
     assert BasicState.try_from_statevector([0.1, 0.1]) == None
