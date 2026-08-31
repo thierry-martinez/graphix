879a0c4c34030b3445365b5320ef0753d15005cc
Thierry Martinez <thierry.martinez@inria.fr>
2026-08-31 09:37:53 +0200

    Fix graphix-qasm-parser and mqtbench rev deps

--- noxfile.py
+++ noxfile.py
@@ -97,7 +97,7 @@ class ReverseDependency:
     [
         ReverseDependency("https://github.com/thierry-martinez/graphix-symbolic", branch="in-place_methods"),
         ReverseDependency("https://github.com/thierry-martinez/graphix-stim-backend", branch="rename-simulate"),
-        ReverseDependency("https://github.com/TeamGraphix/graphix-qasm-parser", branch="refs/pull/16/head"),
+        ReverseDependency("https://github.com/TeamGraphix/graphix-qasm-parser", branch="refs/pull/17/head"),
         ReverseDependency(
             "https://github.com/thierry-martinez/graphix-ibmq", doctest_modules=False, branch="rename-simulate"
         ),
@@ -109,7 +109,7 @@ class ReverseDependency:
             install_target=".[dev]",
             branch="rename-simulate",
         ),
-        ReverseDependency("https://github.com/thierry-martinez/graphix-mqtbench", branch="rename-simulate"),
+        ReverseDependency("https://github.com/TeamGraphix/graphix-mqtbench", branch="refs/pull/4/head"),
     ],
 )
 def tests_reverse_dependencies(session: Session, package: ReverseDependency) -> None:
